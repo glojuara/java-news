d750c949b4f674e45fdf99fedbc2a5af021543ad
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2023-10-29 15:48:46 -0300

    feat: java 13 - JEP 355 [Text Blocks (Preview)]

---
+++ src/main/java/br/com/ornitorrinco/javanews/java13/preview/TextBlocks.java
@@ -0,0 +1,23 @@
+package br.com.ornitorrinco.javanews.java13.preview;
+
+// JEP 355: Text Blocks (Preview)
+public class TextBlocks {
+
+    public static void main(String[] args) {
+
+
+        String html = """
+              <html>
+                  <body>
+                      <p>Hello, world</p>
+                  </body>
+              </html>
+              """;
+
+
+        System.out.println(html);
+
+
+    }
+
+}
