d43c8239b7f8174499a978e8997cdc745ed47a9b
Guilherme Lopes de Araujo <guilherme.xh@hotmail.com>
2023-10-29 16:43:53 -0300

    feat: java 15 - JEP 378 [Text Blocks]

---
+++ src/main/java/br/com/ornitorrinco/javanews/java15/TextBlocks.java
@@ -0,0 +1,40 @@
+package br.com.ornitorrinco.javanews.java15;
+import javax.script.ScriptException;
+
+// JEP 378: Text Blocks
+public class TextBlocks {
+
+
+    /*
+        String::stripIndent(): used to strip away incidental white space from the text block content
+        String::translateEscapes(): used to translate escape sequences
+        String::formatted(Object... args): simplify value substitution in the text block
+    */
+
+
+    public static void main(String[] args) throws ScriptException {
+
+        String html = """
+              <html>\r
+                  <body>\r
+                      <p>Hello, world</p>\r
+                  </body>\r
+              </html>\r
+              """;
+
+        System.out.println(html);
+
+        String source = """
+                public void print(%s object) {
+                    System.out.println(Objects.toString(object));
+                }
+                """.formatted("Object");
+
+        System.out.println(source);
+
+
+
+    }
+
+
+}
